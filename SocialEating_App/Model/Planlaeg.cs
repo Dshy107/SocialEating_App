d1b9d1a1e0636894f5807cb8a3a969c7b101654f
Dshy <KortSkg@KORTSKG-PC>
2016-12-05 11:08:09 +0100

    Lavet Planlaeg

---
+++ SocialEating_App/Model/Planlaeg.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SocialEating_App.Model
+{
+   public class Planlaeg
+    {
+        public string Ret { get; set; }
+        public string ChefKok { get; set; }
+        public string Hjaelper { get; set; }
+        public string Oprydder { get; set; }
+
+        public override string ToString()
+        {
+            return Ret + " \n" + ChefKok + " \n" + Hjaelper + " \n" + Oprydder + "\n";
+        }
+    }
+}
